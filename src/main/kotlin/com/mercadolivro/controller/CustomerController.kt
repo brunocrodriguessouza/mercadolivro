7ae49cf20d2821a789a3de99faa2026484e86b03
Bruno Souza <brunocrodriguessouza>
2021-07-25 13:54:02 -0300

    Branch inicial de uma api desenvolvida em kotlin com spring utilizando lista como o banco de dados

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,44 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.extension.toCustomerModel
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("customer")
+class CustomerController(
+    val customerService: CustomerService
+) {
+
+    @GetMapping
+    fun listAll(@RequestParam name: String?): List<CustomerModel> {
+        return customerService.listAll(name)
+    }
+
+    @GetMapping("/{id}")
+    fun listById(@PathVariable id: String): CustomerModel {
+        return customerService.listById(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customerService.update(customer.toCustomerModel(id))
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customerService.delete(id)
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        customerService.create(customer.toCustomerModel())
+    }
+}
