c5bf2fd72e0d169c156f2a25af5cc160d5cefd59
Bruno Souza <brunocrodriguessouza>
2021-07-30 20:13:12 -0300

    Implementacao para disparar um evento de Purchase(Compra)

--- src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
+++ src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
@@ -2,15 +2,12 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.mapper.PurchaseMapper
 import com.mercadolivro.controller.request.PostPurchaseRequest
-import com.mercadolivro.model.PurchaseModel
 import com.mercadolivro.service.PurchaseService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController("/purchase")
+@RestController
+@RequestMapping("purchase")
 class PurchaseController (
     private val purchaseService: PurchaseService,
     private val purchaseMapper: PurchaseMapper
